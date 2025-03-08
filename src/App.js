4799ac744d9523272e259782a4e4d76c5286f868
JatinSeta <jatinseta7@gmail.com>
2025-03-08 10:01:10 +0530

    Frist file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import Login from './Login&SingUp/Login';
+// import SingUp from './Login&SingUp/SingUp';
+import UseEffeact from './UseEffeact/UseEffeact';
+
+function App() {
+  return (
+   <>
+   <UseEffeact/>
+ {/* <Login/> */}
+ {/* <SingUp/> */}
+   </>
+  );
+}
+
+export default App;
